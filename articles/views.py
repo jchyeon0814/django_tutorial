541aa356f98563dcc13a7c935c07fbd767d701b2
jchyeon0814 <jchyeon0814@gmail.com>
2024-09-18 15:47:19 +0900

    for문 사용

--- articles/views.py
+++ articles/views.py
@@ -6,10 +6,11 @@ def index(request):
     return render(request, 'index.html')
 
 def dinner(request, name):
-    menus = ['족발', '햄버거', '치킨', '초밥']
+    menus = [{"name":'족발',"price":200}, {"name":'햄버거',"price":400}, {"name":'치킨', "price":800}, {"name":'초밥', "price":1000}]
     pick = random.choice(menus)
     context = {
         'pick': pick,
         'name':name,
+        'menus': menus,
     }
     return render(request, 'dinner.html', context)
