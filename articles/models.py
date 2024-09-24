8b6d78654e9f4ba194fdd151bfc5d70d6dd7042b
jchyeon0814 <jchyeon0814@gmail.com>
2024-09-24 19:22:14 +0900

    장르 애니메이션 모델 추가

--- articles/models.py
+++ articles/models.py
@@ -6,4 +6,22 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+class Genre(models.Model):
+    class Meta:
+        db_table = "genre"
     
+    name = models.CharField(max_length=70, default='')
+
+class Animation(models.Model):
+    class Meta:
+        db_table = "animation"
+    
+    title = models.CharField(max_length=70, default='')
+    original_title = models.CharField(max_length=70, default='')
+    genre = models.ManyToManyField(Genre, related_name="animations")
+    company = models.CharField(max_length=70, default='')
+    rated = models.CharField(max_length=70, default='')
+    broadcasted_date = models.CharField(max_length=70, default='')
+    chapters = models.CharField(max_length=70, default='')
+    story = models.TextField(max_length=256, default='')
+    img = models.TextField(max_length=256, default='')
