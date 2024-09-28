be0e62ea4239dc8b710e8d852c7906104644135d
jchyeon0814 <jchyeon0814@gmail.com>
2024-09-28 16:36:18 +0900

    수정화면 추가

--- articles/views.py
+++ articles/views.py
@@ -6,14 +6,13 @@ from .models import Article
 def index(request):
     return render(request, 'index.html')
 
-def dinner(request, name):
+def dinner(request):
     menus = [{"name":'족발',"price":200}, {"name":'햄버거',"price":400}, {"name":'치킨', "price":800}, {"name":'초밥', "price":1000}]
     pick = random.choice(menus)
     articles = Article.objects.order_by('-pk')
 
     context = {
         'pick': pick,
-        'name':name,
         'menus': menus,
         'articles': articles,
     }
@@ -29,4 +28,37 @@ def create_review(request):
     article = Article(title=title, content=content)
     article.save() #DB저장 - Article은 모델을 상속받았기 때문에 save는 부모 모델에 정의되어있음
     
-    return redirect('/articles/dinner/무언가')
+    return redirect('articles:detail', article.pk)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+
+    return render(request, 'detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+
+    if request.method == "POST":
+        article.delete()
+
+    return redirect('articles:dinner')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+
+    return render(request, 'edit.html', context)
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+
+    return redirect('articles:detail', article.pk)
